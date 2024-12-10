40ead54426446afb32ee434a8a225499d986c8a5
Lê Trần Thái Tâm <letranthaitam@gmail.com>
2024-12-10 15:31:42 +0700

    Refactor HomePageTitle component to replace Mantine Title with a semantic h1 element and adjust styling properties🏵💍🚛

--- src/components/homepage/HomepageTitle.tsx
+++ src/components/homepage/HomepageTitle.tsx
@@ -1,4 +1,3 @@
-import { Title } from '@mantine/core'
 import { Space_Grotesk } from 'next/font/google'
 
 const spaceGrotesk = Space_Grotesk({
@@ -11,21 +10,21 @@ const HomePageTitle = () => {
   return (
     <>
       <div className="container mx-auto px-2 pt-3 md:px-24 md:pt-14 grid gap-10">
-        <Title
+        <h1
           style={{
             fontFamily: spaceGrotesk.style.fontFamily,
             fontSmooth: 'auto',
             textTransform: 'uppercase',
             textAlign: 'center',
             letterSpacing: '.9px',
+            fontWeight: 500,
           }}
           className="text-green-800 text-sm leading-6 md:text-2xl md:leading-10"
-          fw={500}
         >
           “Tại Share2Receive, chúng tôi tin rằng mỗi sản phẩm trao đổi không chỉ mang đến niềm vui mới mà còn góp phần
           giữ gìn hành tinh. Hãy cùng nhau xây dựng một thế giới bền vững hơn qua từng lựa chọn nhỏ.” - Xuân Nguyễn,
           Founder
-        </Title>
+        </h1>
       </div>
     </>
   )
