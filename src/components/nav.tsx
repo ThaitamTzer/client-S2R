934331192106d11f2044aedf8ad76b555a2ff1c2
Lê Trần Thái Tâm <letranthaitam@gmail.com>
2024-12-29 05:02:40 +0700

    🏊🎂🌻

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,3 +1,4 @@
+// src/components/authpage/nav.tsx
 'use client'
 
 import clsx from 'clsx'
@@ -10,24 +11,23 @@ const Navigation = ({ navLink }: { navLink: navLink[] }) => {
   const [showHeader, setShowHeader] = useState(true)
   const [showBg, setShowBg] = useState(false)
   const [lastScrollY, setLastScrollY] = useState(0)
-  const scrollThreshold = 5
   const pathname = usePathname()
+  const scrollThreshold = 5
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY
 
-      if (currentScrollY === 0 && pathname === '/') {
-        // Remove background and show header when at the top of the page
-        setShowBg(false)
+      // Always show nav when at top
+      if (currentScrollY <= 0) {
         setShowHeader(true)
         return
       }
 
-      // Only hide/show header if scroll distance exceeds the threshold
+      // Only hide/show nav if scroll amount exceeds threshold
       if (Math.abs(currentScrollY - lastScrollY) > scrollThreshold) {
-        if (currentScrollY > lastScrollY) {
-          // Scrolling down
+        if (currentScrollY > lastScrollY && currentScrollY > 100) {
+          // Scrolling down & not at top
           setShowHeader(false)
           setShowBg(true)
         } else {
@@ -38,47 +38,65 @@ const Navigation = ({ navLink }: { navLink: navLink[] }) => {
       }
     }
 
-    window.addEventListener('scroll', handleScroll, { passive: true })
+    const handleScrollTop = () => {
+      if (pathname === '/' && window.scrollY === 0) {
+        setShowBg(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScrollTop)
+
     return () => {
       window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('scroll', handleScrollTop)
     }
   }, [lastScrollY, pathname])
 
   const isActive = (path: string) => {
+    if (pathname === '/shop') {
+      return path === '/shop'
+    }
     return pathname === path
   }
 
   return (
-    <nav
-      className={clsx(
-        'fixed top-0 z-modal text-green-500 text-lg font-medium w-full transition-transform duration-300 ease-in-out overflow-hidden',
-        {
-          '-translate-y-full': !showHeader, // Hide header when scrolling down
-          'translate-y-[102px] md:translate-y-16': showHeader, // Show header when scrolling up
-          'bg-green-100': showBg, // Add background when not at the top
-          'text-white': !showBg, // Change text color based on background
-        },
-      )}
-    >
-      <div className="container px-1 md:px-44">
-        <ul className="flex flex-row">
-          {navLink.map((link) => (
-            <Link key={link.href} href={link.href}>
-              <li className="p-0 md:text-xl md:px-4 md:py-3 cursor-pointer hover:bg-green-200 hover:text-green-800 px-1 py-3 text-xs font-medium">
-                <p
-                  className={clsx(
-                    'before:none before:left-0 before:right-0 before:-bottom-3 before:mx-auto before:my-0 before:rounded-sm before:h-[1px] md:before:h-[3px] before:bg-green-900 relative',
-                    { 'before:absolute text-green-800': isActive(link.href) },
-                  )}
+    <>
+      <nav
+        className={clsx(
+          'fixed top-0 z-modal text-green-500 text-lg font-medium w-full transition-transform duration-300 overflow-hidden',
+          {
+            '-translate-y-0': !showHeader,
+            'translate-y-[102px]': showHeader,
+            'md:translate-y-16': showHeader,
+            'bg-green-100': showBg,
+            'text-white': !showBg,
+          },
+        )}
+      >
+        <div className="container px-1 md:px-44">
+          <ul className="flex flex-row">
+            {navLink.map((link) => (
+              <Link key={link.href} href={link.href}>
+                <li
+                  key={link.href}
+                  className="p-0 md:text-xl md:px-4 md:py-3 cursor-pointer hover:bg-green-200 hover:text-green-800 px-1 py-3 text-xs font-medium"
                 >
-                  {link.label}
-                </p>
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </div>
-    </nav>
+                  <p
+                    className={clsx(
+                      'before:none before:left-0 before:right-0  before:-bottom-3 before:mx-auto before:my-0 before:rounded-sm before:h-[1px] md:before:h-[3px] before:bg-green-900 relative',
+                      { 'before:absolute text-green-800': isActive(link.href) },
+                    )}
+                  >
+                    {link.label}
+                  </p>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 
